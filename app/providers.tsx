0c7ee2f00dd4f06421986a7d59a4b5b219137aa4
Hmtgit7 <hmtloharcoding3579@gmail.com>
2025-05-22 13:50:59 +0530

    app with all functionality

---
+++ app/providers.tsx
@@ -0,0 +1,16 @@
+// chat-app/app/providers.tsx
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react';
+import { ToastProvider } from './components/ui/ToastContext';
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <SessionProvider>
+      <ToastProvider>
+        {children}
+      </ToastProvider>
+    </SessionProvider>
+  );
+} 
